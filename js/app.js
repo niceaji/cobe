b999ae62e7fc89a282c1c4b49ea5de57f362fe25
aji <a.ji@daum.net>
2013-05-16 00:50:35 +0900

    backbonejs start

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var ItemModel = Backbone.Model.extend({
+
+	
+});
+var ItemCollection = Backbone.Collection.extend({
+	model : ItemModel
+});
+var ItemView = Backbone.View.extend({
+
+	// el : $('.item-wrap'),
+	template : _.template($('#itemViewTemplate').html()),
+
+	initialize : function(){
+
+		this.collection = new ItemCollection();
+		this.listenTo(this.collection, "reset", this.render );
+		// this.listenTo(this.model, "add" , this.render);
+		
+		this.collection.url = 'data/20130515.js';
+		this.collection.fetch({reset:true});
+
+
+	},
+	render :function(){
+
+		console.log(arguments)
+
+		this.$el.append( this.template() );
+
+
+		return this;
+
+
+	}
+
+});
+var AppView = Backbone.View.extend({
+
+
+});
+
+new ItemView();
