2695cd80f395ca7dc1765ecb9080e62fe86a30af
niceaji <a.ji@daum.net>
2013-05-21 13:10:00 +0900

    update Data!

--- data/2013-05-21.js
+++ data/2013-05-21.js
@@ -5,11 +5,12 @@
     "userid": "99XBcrBf_4Q0",
     "content": "이게 베충이들 현실 ㅋㅋㅋㅋㅋ\r\n\r\n저게 퍼포먼스라고 쥐랄를 떨어 싸는데...그럼 왜 자수함? ㅋ",
     "time": 1369103370000,
-    "recommendCount": 2122,
-    "disagreeCount": 38,
-    "childCount": 57,
+    "recommendCount": 3076,
+    "disagreeCount": 56,
+    "childCount": 78,
     "newsid": "20130521110324257",
-    "newsTitle": "盧 전 대통령 희화화 사진 일베에 올린 고교생 자수"
+    "newsTitle": "盧 전 대통령 희화화 사진 일베에 올린 고교생 자수",
+    "newsDate": "20130521110324"
   },
   {
     "id": 176691158,
@@ -17,11 +18,12 @@
     "userid": "Fyy-SY93Tuw0",
     "content": "국정원 1년 예산이 원칙적으론 비공개로 알수 없지만.\r\n대략 9천억원..1조원 가까이 된답니다..헉..\r\n국민의 혈세를 받아 쳐먹어 가면서 고작 하는일이\r\n댓글이나 달고 정치개입이나 하고..또 뭐 일베충들을 초청한다고\r\n하여튼 10 색히.들\r\n",
     "time": 1369098037000,
-    "recommendCount": 1766,
-    "disagreeCount": 32,
-    "childCount": 46,
+    "recommendCount": 2162,
+    "disagreeCount": 39,
+    "childCount": 50,
     "newsid": "20130521095013278",
-    "newsTitle": "박원순 시장 “국정원 문건은 1000만 서울시민 모독한 것”"
+    "newsTitle": "박원순 시장 “국정원 문건은 1000만 서울시민 모독한 것”",
+    "newsDate": "20130521095013"
   },
   {
     "id": 176685731,
@@ -29,11 +31,12 @@
     "userid": "3Is3Mdz1TCk0",
     "content": "광주시민들을 학살 강간 시체유기하고\r\n민주화시켰다 전두환의 역사에 길이남을\r\n망언.. 이걸 전효성이 웃으면서 말했다\r\n우리들의 기가막힌심정을 과연 진심으로\r\n깨달앗을지 의문이다",
     "time": 1369095887000,
-    "recommendCount": 1104,
-    "disagreeCount": 73,
-    "childCount": 23,
+    "recommendCount": 1305,
+    "disagreeCount": 83,
+    "childCount": 31,
     "newsid": "20130521091520100",
-    "newsTitle": "전효성, 눈물 사과에 네티즌들 엇갈린 시선"
+    "newsTitle": "전효성, 눈물 사과에 네티즌들 엇갈린 시선",
+    "newsDate": "20130521091520"
   },
   {
     "id": 176678386,
@@ -41,11 +44,12 @@
     "userid": "gtFzvc6IzBA0",
     "content": "기회가 있었는데, 행사철 되니까 이제서야 공개석상에서 사과... 실망스럽다...",
     "time": 1369092407000,
-    "recommendCount": 1562,
-    "disagreeCount": 82,
-    "childCount": 47,
+    "recommendCount": 1725,
+    "disagreeCount": 96,
+    "childCount": 53,
     "newsid": "20130521081611761",
-    "newsTitle": "전효성, 카이스트 공연서 눈물 속 사과 “깊이 반성 중”"
+    "newsTitle": "전효성, 카이스트 공연서 눈물 속 사과 “깊이 반성 중”",
+    "newsDate": "20130521081611"
   },
   {
     "id": 176670686,
@@ -65,11 +69,12 @@
     "userid": "RCQJHtYw_Zk0",
     "content": "윤창중 보내주고 \r\n\r\n이명박 전직 대통령 보내 달라 해야겠다. \r\n",
     "time": 1369084968000,
-    "recommendCount": 2747,
+    "recommendCount": 2786,
     "disagreeCount": 44,
-    "childCount": 42,
+    "childCount": 43,
     "newsid": "20130521023105462",
-    "newsTitle": "[단독] \"혐의내용 경·중범죄 상관없이 윤창중 체포영장 발부할 것\""
+    "newsTitle": "[단독] \"혐의내용 경·중범죄 상관없이 윤창중 체포영장 발부할 것\"",
+    "newsDate": "20130521023105"
   },
   {
     "id": 176669766,
@@ -77,11 +82,12 @@
     "userid": "JhU5c3GQ38c0",
     "content": "저런 더러운새끼...국회는 뭐하나? 한통속이라서 법으로 막지 않았겠지. ",
     "time": 1369082932000,
-    "recommendCount": 1740,
+    "recommendCount": 1769,
     "disagreeCount": 8,
-    "childCount": 23,
+    "childCount": 24,
     "newsid": "20130521051004377",
-    "newsTitle": "[단독]'오리온 수사지휘' 법무장관, 오리온 취직"
+    "newsTitle": "[단독]'오리온 수사지휘' 법무장관, 오리온 취직",
+    "newsDate": "20130521051004"
   },
   {
     "id": 176642436,
@@ -89,10 +95,11 @@
     "userid": "SaUcnz.Pa8A0",
     "content": "헐국정원이일베 ㅋㅋㅋㅋ골때리는나라다",
     "time": 1369053806000,
-    "recommendCount": 8495,
-    "disagreeCount": 161,
-    "childCount": 262,
+    "recommendCount": 8639,
+    "disagreeCount": 166,
+    "childCount": 265,
     "newsid": "20130520213007425",
-    "newsTitle": "국정원, 일베회원 등 보수 누리꾼 안보행사 초청 논란"
+    "newsTitle": "국정원, 일베회원 등 보수 누리꾼 안보행사 초청 논란",
+    "newsDate": "20130520213007"
   }
 ]
