fa4f38e32100a30da5b63ef674075e50aa1c3208
niceaji <a.ji@daum.net>
2013-05-23 08:12:01 +0900

    update Data!

--- data/2013-05-23.js
+++ data/2013-05-23.js
@@ -6,9 +6,9 @@
     "content": "수뢰공무원이 인권타령?\r\n\r\n셀프 감금에 버금가는 셀프 탄압..",
     "time": 1369235962000,
     "dateStr": "2013-05-23T00:19:22+09:00",
-    "recommendCount": 695,
+    "recommendCount": 720,
     "disagreeCount": 13,
-    "childCount": 3,
+    "childCount": 4,
     "newsid": "20130523001505742",
     "newsTitle": "“박원순 서울시장이 불법사찰 지시”",
     "newsDate": "20130523001505"
