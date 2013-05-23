97efe132657dfeec06dcc9e4d1f5de4699c9cf49
niceaji <a.ji@daum.net>
2013-05-24 06:12:01 +0900

    update Data!

--- data/2013-05-24.js
+++ data/2013-05-24.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 177128877,
+    "name": "노을지는 바다",
+    "userid": "vgqxb77NdoU0",
+    "content": "개나소나 다 정치냐 양아치야",
+    "time": 1369322593000,
+    "dateStr": "2013-05-24T00:23:13+09:00",
+    "recommendCount": 516,
+    "disagreeCount": 12,
+    "childCount": 2,
+    "newsid": "20130524001406755",
+    "newsTitle": "'썰전' 김흥국, 정치 향한 꿈 고백 \"매일 연락 기다려\"",
+    "newsDate": "20130524001406"
+  }
+]
