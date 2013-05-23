bd3ae1976953ebffb3e3e44b7e0616e796e20dad
niceaji <a.ji@daum.net>
2013-05-24 07:30:01 +0900

    update Data!

--- data/2013-05-24.js
+++ data/2013-05-24.js
@@ -6,11 +6,25 @@
     "content": "개나소나 다 정치냐 양아치야",
     "time": 1369322593000,
     "dateStr": "2013-05-24T00:23:13+09:00",
-    "recommendCount": 779,
+    "recommendCount": 806,
     "disagreeCount": 17,
     "childCount": 4,
     "newsid": "20130524001406755",
     "newsTitle": "'썰전' 김흥국, 정치 향한 꿈 고백 \"매일 연락 기다려\"",
     "newsDate": "20130524001406"
+  },
+  {
+    "id": 177128616,
+    "name": "용시엔",
+    "userid": "882ktwkJZaM0",
+    "content": "그 제적생은 훗날 국민MC가 됩니다..",
+    "time": 1369322388000,
+    "dateStr": "2013-05-24T00:19:48+09:00",
+    "recommendCount": 538,
+    "disagreeCount": 32,
+    "childCount": 3,
+    "newsid": "20130523233806456",
+    "newsTitle": "유재석 “대학시절 공채개그맨 합격, 제적당했다”(해투3)",
+    "newsDate": "20130523233806"
   }
 ]
