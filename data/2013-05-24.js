199143d18d71e86a54f42d5de5ea37653061f4a9
niceaji <a.ji@daum.net>
2013-05-24 06:45:02 +0900

    update Data!

--- data/2013-05-24.js
+++ data/2013-05-24.js
@@ -6,9 +6,9 @@
     "content": "개나소나 다 정치냐 양아치야",
     "time": 1369322593000,
     "dateStr": "2013-05-24T00:23:13+09:00",
-    "recommendCount": 595,
+    "recommendCount": 608,
     "disagreeCount": 13,
-    "childCount": 3,
+    "childCount": 4,
     "newsid": "20130524001406755",
     "newsTitle": "'썰전' 김흥국, 정치 향한 꿈 고백 \"매일 연락 기다려\"",
     "newsDate": "20130524001406"
