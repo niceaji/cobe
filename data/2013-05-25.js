dd335d2c8384a53ef307ecf8f4d643f674616f1b
niceaji <a.ji@daum.net>
2013-05-25 03:39:02 +0900

    update Data!

--- data/2013-05-25.js
+++ data/2013-05-25.js
@@ -6,9 +6,9 @@
     "content": "표현의자유는 좋지만\r\n허위사실유표로 잘못된 역사인식을 심어주잖아 \r\n나라를 혼란시키는데 자유가어딨어",
     "time": 1369409854000,
     "dateStr": "2013-05-25T00:37:34+09:00",
-    "recommendCount": 635,
+    "recommendCount": 638,
     "disagreeCount": 61,
-    "childCount": 98,
+    "childCount": 99,
     "newsid": "20130524223306816",
     "newsTitle": "‘일베 폐쇄’ 표현의 자유 논란",
     "newsDate": "20130524223306"
@@ -20,9 +20,9 @@
     "content": "진짜 부정선거 였구나~\r\n\r\n뭐 수장만 나는 몰랐다식으로\r\n\r\n끝나겠지 우리나라에 정의란\r\n\r\n과연 아직 살아있는가?",
     "time": 1369408140000,
     "dateStr": "2013-05-25T00:09:00+09:00",
-    "recommendCount": 1511,
+    "recommendCount": 1515,
     "disagreeCount": 10,
-    "childCount": 41,
+    "childCount": 43,
     "newsid": "20130524230406076",
     "newsTitle": "서울경찰청 '국정원 수사' 데이터삭제 증거인멸 시도",
     "newsDate": "20130524230406"
