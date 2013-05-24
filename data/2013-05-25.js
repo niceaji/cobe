6fa9a9559615292f151c9c0eda4e532d8a03a839
niceaji <a.ji@daum.net>
2013-05-25 02:33:02 +0900

    update Data!

--- data/2013-05-25.js
+++ data/2013-05-25.js
@@ -6,9 +6,9 @@
     "content": "진짜 부정선거 였구나~\r\n\r\n뭐 수장만 나는 몰랐다식으로\r\n\r\n끝나겠지 우리나라에 정의란\r\n\r\n과연 아직 살아있는가?",
     "time": 1369408140000,
     "dateStr": "2013-05-25T00:09:00+09:00",
-    "recommendCount": 1256,
+    "recommendCount": 1271,
     "disagreeCount": 7,
-    "childCount": 34,
+    "childCount": 35,
     "newsid": "20130524230406076",
     "newsTitle": "서울경찰청 '국정원 수사' 데이터삭제 증거인멸 시도",
     "newsDate": "20130524230406"
