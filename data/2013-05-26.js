f8189a4b8a9abd120f381901ef912412048e0545
niceaji <a.ji@daum.net>
2013-05-26 08:27:01 +0900

    update Data!

--- data/2013-05-26.js
+++ data/2013-05-26.js
@@ -1,12 +1,26 @@
 [
   {
+    "id": 177391493,
+    "name": "daumcho",
+    "userid": "gUYO3h41uAE0",
+    "content": "꼬박꼬박 이자갚는 나같은 놈은 바보된겨?",
+    "time": 1369519945000,
+    "dateStr": "2013-05-26T07:12:25+09:00",
+    "recommendCount": 528,
+    "disagreeCount": 7,
+    "childCount": 14,
+    "newsid": "20130526060305877",
+    "newsTitle": "'개인빚 자꾸 갚아주는 나라'…올해만 100만명 구제",
+    "newsDate": "20130526060305"
+  },
+  {
     "id": 177377619,
     "name": "mjh1207",
     "userid": "2HVDTbvH9OQ0",
     "content": "미국 영국 프랑스 한국\r\n모든 나라에서 전쟁중에 성폭력이 있었을 가능성이 있지만\r\n그걸 나라와 군이 주도해서 장려한 나라는\r\n세계에서 일본이 유일하다",
     "time": 1369494916000,
     "dateStr": "2013-05-26T00:15:16+09:00",
-    "recommendCount": 1056,
+    "recommendCount": 1069,
     "disagreeCount": 15,
     "childCount": 3,
     "newsid": "20130525235906561",
