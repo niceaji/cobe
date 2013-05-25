d15df3ab65e80f56ec1b2fba73e887ac660669c6
niceaji <a.ji@daum.net>
2013-05-26 03:42:02 +0900

    update Data!

--- data/2013-05-26.js
+++ data/2013-05-26.js
@@ -6,7 +6,7 @@
     "content": "미국 영국 프랑스 한국\r\n모든 나라에서 전쟁중에 성폭력이 있었을 가능성이 있지만\r\n그걸 나라와 군이 주도해서 장려한 나라는\r\n세계에서 일본이 유일하다",
     "time": 1369494916000,
     "dateStr": "2013-05-26T00:15:16+09:00",
-    "recommendCount": 547,
+    "recommendCount": 548,
     "disagreeCount": 8,
     "childCount": 2,
     "newsid": "20130525235906561",
