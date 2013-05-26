2f9315863c63b81169dd9887196d486ce683ca1e
niceaji <a.ji@daum.net>
2013-05-27 07:15:02 +0900

    update Data!

--- data/2013-05-27.js
+++ data/2013-05-27.js
@@ -6,9 +6,9 @@
     "content": "박그네의 종주국 미국서\r\nCIA가 선거개입 여론조작, 조직적 부정선거\r\nFBI가 은폐조작, 증거인멸 했다면\r\n\r\n발칵 뒤집힌다\r\n전 대통령 수사. 현 대통령 사임감 ☞  추천\r\n\r\n그냥 넘어간다 ☞   비추천",
     "time": 1369602742000,
     "dateStr": "2013-05-27T06:12:22+09:00",
-    "recommendCount": 655,
-    "disagreeCount": 10,
-    "childCount": 7,
+    "recommendCount": 695,
+    "disagreeCount": 11,
+    "childCount": 8,
     "newsid": "20130527034106967",
     "newsTitle": "'댓글 여성 수사' 경찰 간부가 컴퓨터 데이터 삭제",
     "newsDate": "20130527034106"
