6ca5fdfaeb2500ac2a239b5198943267fb4787f3
niceaji <a.ji@daum.net>
2013-05-27 08:06:03 +0900

    update Data!

--- data/2013-05-27.js
+++ data/2013-05-27.js
@@ -1,17 +1,17 @@
 [
   {
-    "id": 177492555,
-    "name": "younggyo",
-    "userid": "dEfR7ovO.ho0",
-    "content": "쓰레기같은대통령은 국민은 존중하지않는다.\r\n이명박은 대통령이아니었다.\r\n나라살림 망쳐놓은 역적배 .\r\n왜?국민의존경을 못받지는 본인도 알것이다.\r\n지금이라도 국민앞에 사죄하고\r\n용서를구하라.",
-    "time": 1369602836000,
-    "dateStr": "2013-05-27T06:13:56+09:00",
-    "recommendCount": 859,
-    "disagreeCount": 207,
-    "childCount": 36,
-    "newsid": "20130527022216250",
-    "newsTitle": "MB, 노무현 前대통령 4주기 추도식날 옛 참모들과 골프 회동 논란",
-    "newsDate": "20130527022216"
+    "id": 177495634,
+    "name": "우린하나야",
+    "userid": "xzsoXwePvHg0",
+    "content": "운동선수들 못배운거티내냐는말이 어울리는듯.. 스포츠아나운서얼마나 만만하게봤으면저럴까",
+    "time": 1369606894000,
+    "dateStr": "2013-05-27T07:21:34+09:00",
+    "recommendCount": 584,
+    "disagreeCount": 45,
+    "childCount": 9,
+    "newsid": "20130527070337124",
+    "newsTitle": "임찬규-정인영 물벼락 사건에 네티즌 ‘부글부글’",
+    "newsDate": "20130527070337"
   },
   {
     "id": 177492508,
@@ -20,11 +20,25 @@
     "content": "박그네의 종주국 미국서\r\nCIA가 선거개입 여론조작, 조직적 부정선거\r\nFBI가 은폐조작, 증거인멸 했다면\r\n\r\n발칵 뒤집힌다\r\n전 대통령 수사. 현 대통령 사임감 ☞  추천\r\n\r\n그냥 넘어간다 ☞   비추천",
     "time": 1369602742000,
     "dateStr": "2013-05-27T06:12:22+09:00",
-    "recommendCount": 1416,
-    "disagreeCount": 17,
-    "childCount": 25,
+    "recommendCount": 1654,
+    "disagreeCount": 20,
+    "childCount": 28,
     "newsid": "20130527034106967",
     "newsTitle": "'댓글 여성 수사' 경찰 간부가 컴퓨터 데이터 삭제",
     "newsDate": "20130527034106"
+  },
+  {
+    "id": 177492504,
+    "name": "maru",
+    "userid": "NT6EhQp27tQ0",
+    "content": "바랄걸바라세요  mb에게  그나저나 윤창중이는 언제미국보내나?",
+    "time": 1369602731000,
+    "dateStr": "2013-05-27T06:12:11+09:00",
+    "recommendCount": 1005,
+    "disagreeCount": 150,
+    "childCount": 15,
+    "newsid": "20130527022216250",
+    "newsTitle": "MB, 노무현 前대통령 4주기 추도식날 옛 참모들과 골프 회동 논란",
+    "newsDate": "20130527022216"
   }
 ]
