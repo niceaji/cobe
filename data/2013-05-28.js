5966e072b582bc6841d094f6eebd101c31130fd4
niceaji <a.ji@daum.net>
2013-05-28 08:00:02 +0900

    update Data!

--- data/2013-05-28.js
+++ data/2013-05-28.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 177650940,
+    "name": "tiger j k",
+    "userid": "O3LOKWwKrus0",
+    "content": "역시 쿨하네ㅋ예전 방송작가가 꼽은 성격제일 쿨하고 좋은 연예인 여자부문1위.",
+    "time": 1369670195000,
+    "dateStr": "2013-05-28T00:56:35+09:00",
+    "recommendCount": 562,
+    "disagreeCount": 54,
+    "childCount": 5,
+    "newsid": "20130528004706472",
+    "newsTitle": "이소라 열애고백 “연하 남친과 교제, 한혜진 만큼 운 좋다”",
+    "newsDate": "20130528004706"
+  }
+]
