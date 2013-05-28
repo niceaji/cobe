0d4fcf9042bf896ab51aa5cdfac4e1a804482310
niceaji <a.ji@daum.net>
2013-05-29 07:09:02 +0900

    update Data!

--- data/2013-05-29.js
+++ data/2013-05-29.js
@@ -6,9 +6,9 @@
     "content": "가정불화루머?여기서 처음 듣는다. 누가 만든 루머야?",
     "time": 1369753389000,
     "dateStr": "2013-05-29T00:03:09+09:00",
-    "recommendCount": 565,
-    "disagreeCount": 29,
-    "childCount": 7,
+    "recommendCount": 567,
+    "disagreeCount": 30,
+    "childCount": 8,
     "newsid": "20130528234103705",
     "newsTitle": "'화신' 장혁, 가정 불화 루머 입 열었다… \"가정 보단 촬영장?\"",
     "newsDate": "20130528234103"
