e497c47b8cad896eab04b0b1723ba74f944ef8e5
niceaji <a.ji@daum.net>
2013-05-30 07:24:03 +0900

    update Data!

--- data/2013-05-30.js
+++ data/2013-05-30.js
@@ -1,14 +1,28 @@
 [
   {
+    "id": 177986069,
+    "name": "Dionysos",
+    "userid": "5v9E-k7DTL10",
+    "content": "결국 연예인도 화류계가 발전한거란걸 너무 명확하게 보여주는거 아냐..\r\n\r\n돈만 많으면 TV 든 영화든 어서 날고기는 애라도 다 부를 수 있다는거군..",
+    "time": 1369842541000,
+    "dateStr": "2013-05-30T00:49:01+09:00",
+    "recommendCount": 508,
+    "disagreeCount": 7,
+    "childCount": 6,
+    "newsid": "20130529112408222",
+    "newsTitle": "<해외 문화> 하룻밤 대가 3600만원?…홍콩연예계 파문",
+    "newsDate": "20130529112408"
+  },
+  {
     "id": 177982341,
     "name": "김은영",
     "userid": "vMkqr1uGPzo0",
     "content": "솔직해서 보기 좋습니다 . 팬은 아니었는데 팬이 될것 같네요. 앞으로도 지금처럼 당당하고 열심히 살아주세요 화이팅!",
     "time": 1369839964000,
     "dateStr": "2013-05-30T00:06:04+09:00",
-    "recommendCount": 1666,
-    "disagreeCount": 202,
-    "childCount": 15,
+    "recommendCount": 1685,
+    "disagreeCount": 205,
+    "childCount": 17,
     "newsid": "20130529235105485",
     "newsTitle": "이효리 밝힌 상업광고 거부 이유 \"솔직하고 싶었다\"",
     "newsDate": "20130529235105"
