7e2004ede4b197b3a2bf2f540520d812f68e4c97
niceaji <a.ji@daum.net>
2013-05-31 04:03:02 +0900

    update Data!

--- data/2013-05-31.js
+++ data/2013-05-31.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 178145326,
+    "name": "7번방의가구",
+    "userid": "YpLD8ob7HoQ0",
+    "content": "자꾸 이철희소장님이 좋아진다~~~",
+    "time": 1369927316000,
+    "dateStr": "2013-05-31T00:21:56+09:00",
+    "recommendCount": 543,
+    "disagreeCount": 41,
+    "childCount": 13,
+    "newsid": "20130531000905168",
+    "newsTitle": "'썰전' 강용석 VS 이철희, 날선 논쟁..김구라 '난처'",
+    "newsDate": "20130531000905"
+  }
+]
