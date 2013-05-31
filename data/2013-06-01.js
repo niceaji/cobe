bebf8dbf948fdbb7a83490c61f7ff467358c7c0a
niceaji <a.ji@daum.net>
2013-06-01 02:33:02 +0900

    update Data!

--- data/2013-06-01.js
+++ data/2013-06-01.js
@@ -1,14 +1,28 @@
 [
   {
+    "id": 178317265,
+    "name": "혼또니카와잉",
+    "userid": "78vStIiJ4iE0",
+    "content": "가요프로 순위 어차피 권위도 없는데   \r\n\r\n\r\n가수들은 그런거에 집착하구나..\r\n\r\n대중들은 관심없는데",
+    "time": 1370013632000,
+    "dateStr": "2013-06-01T00:20:32+09:00",
+    "recommendCount": 533,
+    "disagreeCount": 24,
+    "childCount": 5,
+    "newsid": "20130531235706097",
+    "newsTitle": "이효리 \"순위프로 소외감 느끼고 마음 안편해, 아이돌에 안된다\" 고백",
+    "newsDate": "20130531235706"
+  },
+  {
     "id": 178315347,
     "name": "대한국인",
     "userid": "yUE8_e7vHL50",
     "content": "그럼지금까지 왜 가만있었냐. ",
     "time": 1370012530000,
     "dateStr": "2013-06-01T00:02:10+09:00",
-    "recommendCount": 863,
+    "recommendCount": 868,
     "disagreeCount": 63,
-    "childCount": 51,
+    "childCount": 52,
     "newsid": "20130531235709105",
     "newsTitle": "여대생 살해 용의 택시기사 검거..혐의 부인(종합)",
     "newsDate": "20130531235709"
