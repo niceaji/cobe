9bdef8056da7cbb36e32a032eca808226ae18677
niceaji <a.ji@daum.net>
2013-06-01 01:48:02 +0900

    update Data!

--- data/2013-06-01.js
+++ data/2013-06-01.js
@@ -6,9 +6,9 @@
     "content": "그럼지금까지 왜 가만있었냐. ",
     "time": 1370012530000,
     "dateStr": "2013-06-01T00:02:10+09:00",
-    "recommendCount": 700,
-    "disagreeCount": 47,
-    "childCount": 44,
+    "recommendCount": 709,
+    "disagreeCount": 48,
+    "childCount": 45,
     "newsid": "20130531235709105",
     "newsTitle": "여대생 살해 용의 택시기사 검거..혐의 부인(종합)",
     "newsDate": "20130531235709"
