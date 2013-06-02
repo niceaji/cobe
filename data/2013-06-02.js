690f88cc618c0aa6d6153764c308358721443378
niceaji <a.ji@daum.net>
2013-06-02 10:06:01 +0900

    update Data!

--- data/2013-06-02.js
+++ data/2013-06-02.js
@@ -6,11 +6,25 @@
     "content": "뭐가 아찔한 패션이야? 난 쟤  다시 보니까  아찔하다  ㅡ.ㅡ",
     "time": 1370129176000,
     "dateStr": "2013-06-02T08:26:16+09:00",
-    "recommendCount": 565,
+    "recommendCount": 588,
     "disagreeCount": 7,
     "childCount": 8,
     "newsid": "20130602080704914",
     "newsTitle": "[포토엔]티아라 은정 '아찔한 노출 패션'(공항패션)",
     "newsDate": "20130602080704"
+  },
+  {
+    "id": 178456896,
+    "name": "자업자득",
+    "userid": "z9-6EgB8DtQ0",
+    "content": "남은 인생은 행복만 가득할거에요  그동안 참고 인내한 보람 꼭 있길!!",
+    "time": 1370129084000,
+    "dateStr": "2013-06-02T08:24:44+09:00",
+    "recommendCount": 533,
+    "disagreeCount": 30,
+    "childCount": 5,
+    "newsid": "20130602081604956",
+    "newsTitle": "장윤정 \"결혼준비 이렇게 복잡한지 몰랐다, 두번은 못가\"",
+    "newsDate": "20130602081604"
   }
 ]
