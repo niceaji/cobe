f4ddfaa739cb49c2806b454fb7e13717f9fd7aa9
niceaji <a.ji@daum.net>
2013-06-02 10:09:02 +0900

    update Data!

--- data/2013-06-02.js
+++ data/2013-06-02.js
@@ -6,7 +6,7 @@
     "content": "뭐가 아찔한 패션이야? 난 쟤  다시 보니까  아찔하다  ㅡ.ㅡ",
     "time": 1370129176000,
     "dateStr": "2013-06-02T08:26:16+09:00",
-    "recommendCount": 588,
+    "recommendCount": 603,
     "disagreeCount": 7,
     "childCount": 8,
     "newsid": "20130602080704914",
@@ -20,11 +20,25 @@
     "content": "남은 인생은 행복만 가득할거에요  그동안 참고 인내한 보람 꼭 있길!!",
     "time": 1370129084000,
     "dateStr": "2013-06-02T08:24:44+09:00",
-    "recommendCount": 533,
+    "recommendCount": 547,
     "disagreeCount": 30,
-    "childCount": 5,
+    "childCount": 6,
     "newsid": "20130602081604956",
     "newsTitle": "장윤정 \"결혼준비 이렇게 복잡한지 몰랐다, 두번은 못가\"",
     "newsDate": "20130602081604"
+  },
+  {
+    "id": 178454418,
+    "name": "암행어사",
+    "userid": "e872gvMokzo0",
+    "content": "원세훈. 국정윈 댓글녀...\r\n창중이  빤스..",
+    "time": 1370125711000,
+    "dateStr": "2013-06-02T07:28:31+09:00",
+    "recommendCount": 520,
+    "disagreeCount": 8,
+    "childCount": 25,
+    "newsid": "20130602060109345",
+    "newsTitle": "<새정부 100일>① 국정운영 성적표",
+    "newsDate": "20130602060109"
   }
 ]
