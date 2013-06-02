1d69865b0457570907afd21ea2b23de1dbf4fc91
niceaji <a.ji@daum.net>
2013-06-03 08:00:07 +0900

    update Data!

--- data/2013-06-03.js
+++ data/2013-06-03.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 178558377,
+    "name": "ehreo",
+    "userid": "dULvocLqwk50",
+    "content": "카니발운전자가 안됬다.",
+    "time": 1370207661000,
+    "dateStr": "2013-06-03T06:14:21+09:00",
+    "recommendCount": 505,
+    "disagreeCount": 5,
+    "childCount": 2,
+    "newsid": "20130603033706767",
+    "newsTitle": "블랙박스에 잡힌 심야 5중 충돌의 진실",
+    "newsDate": "20130603033706"
+  }
+]
