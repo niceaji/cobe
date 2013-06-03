d0b8cae39c480b3514b52ccabc1f1e8d60b6234c
niceaji <a.ji@daum.net>
2013-06-04 06:51:02 +0900

    update Data!

--- data/2013-06-04.js
+++ data/2013-06-04.js
@@ -6,7 +6,7 @@
     "content": "그렇게 몸에  심한변화를  준다면\r\n당신은 오래살지 못할꺼다에\r\n한표던집니다\r\n저거 고민맞네 부인입장에서",
     "time": 1370273820000,
     "dateStr": "2013-06-04T00:37:00+09:00",
-    "recommendCount": 537,
+    "recommendCount": 541,
     "disagreeCount": 7,
     "childCount": 0,
     "newsid": "20130604002507609",
