873def0fe3e5995f3ce4ac601161d333ca1a2951
niceaji <a.ji@daum.net>
2013-06-05 05:12:01 +0900

    update Data!

--- data/2013-06-05.js
+++ data/2013-06-05.js
@@ -1,12 +1,26 @@
 [
   {
+    "id": 178859051,
+    "name": "derek",
+    "userid": "4Xag9y5oEUE0",
+    "content": "최강희 좀 짜르자 진짜...............\r\n진심이다.........\r\n봐줄만큼 봐주지 않았냐????",
+    "time": 1370374115000,
+    "dateStr": "2013-06-05T04:28:35+09:00",
+    "recommendCount": 551,
+    "disagreeCount": 50,
+    "childCount": 54,
+    "newsid": "20130605042703965",
+    "newsTitle": "'김치우가 살렸다' 한국, 레바논에 간신히 무승부..조 1위 도약",
+    "newsDate": "20130605042703"
+  },
+  {
     "id": 178846313,
     "name": "net256",
     "userid": "i8W9SCMq8NY0",
     "content": "하기싫은거 자꾸시키면 짜증나긴하지",
     "time": 1370358522000,
     "dateStr": "2013-06-05T00:08:42+09:00",
-    "recommendCount": 1063,
+    "recommendCount": 1065,
     "disagreeCount": 152,
     "childCount": 21,
     "newsid": "20130604235806204",
