6f1760216c08254fcd088cbd5a9b9ac83d801c13
niceaji <a.ji@daum.net>
2013-06-05 03:09:01 +0900

    update Data!

--- data/2013-06-05.js
+++ data/2013-06-05.js
@@ -6,9 +6,9 @@
     "content": "하기싫은거 자꾸시키면 짜증나긴하지",
     "time": 1370358522000,
     "dateStr": "2013-06-05T00:08:42+09:00",
-    "recommendCount": 920,
-    "disagreeCount": 127,
-    "childCount": 18,
+    "recommendCount": 924,
+    "disagreeCount": 129,
+    "childCount": 19,
     "newsid": "20130604235806204",
     "newsTitle": "이종석 \"아이유 불화설? 분장 아이디어 제의 짜증\" 솔직고백",
     "newsDate": "20130604235806"
