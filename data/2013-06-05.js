575d902b1d15d9e2fd114aafb1c8308238d6810b
niceaji <a.ji@daum.net>
2013-06-05 04:51:03 +0900

    update Data!

--- data/2013-06-05.js
+++ data/2013-06-05.js
@@ -6,7 +6,7 @@
     "content": "하기싫은거 자꾸시키면 짜증나긴하지",
     "time": 1370358522000,
     "dateStr": "2013-06-05T00:08:42+09:00",
-    "recommendCount": 1048,
+    "recommendCount": 1051,
     "disagreeCount": 148,
     "childCount": 21,
     "newsid": "20130604235806204",
