9b150a56986bba8a0a53da0b6e56fbc0875f51a3
niceaji <a.ji@daum.net>
2013-06-06 04:33:02 +0900

    update Data!

--- data/2013-06-06.js
+++ data/2013-06-06.js
@@ -6,9 +6,9 @@
     "content": "아쉬울게 없자나, 괜히 올인하면 끝나고 관계정리 불편해지자나. 어차피 목적은 달성했지 ㅋㅋㅋ\r\n쟤가 밖에서 남자 못만나서 여기 나왔을까?ㅋㅋㅋ",
     "time": 1370445644000,
     "dateStr": "2013-06-06T00:20:44+09:00",
-    "recommendCount": 578,
+    "recommendCount": 580,
     "disagreeCount": 11,
-    "childCount": 10,
+    "childCount": 11,
     "newsid": "20130606001506524",
     "newsTitle": "'짝' 어장관리 여자1호, 남자들에 유감 샀다",
     "newsDate": "20130606001506"
