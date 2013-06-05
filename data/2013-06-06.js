8750f6b926987d6afba2e8c7eac981d5f0b6238c
niceaji <a.ji@daum.net>
2013-06-06 05:21:02 +0900

    update Data!

--- data/2013-06-06.js
+++ data/2013-06-06.js
@@ -6,7 +6,7 @@
     "content": "아쉬울게 없자나, 괜히 올인하면 끝나고 관계정리 불편해지자나. 어차피 목적은 달성했지 ㅋㅋㅋ\r\n쟤가 밖에서 남자 못만나서 여기 나왔을까?ㅋㅋㅋ",
     "time": 1370445644000,
     "dateStr": "2013-06-06T00:20:44+09:00",
-    "recommendCount": 619,
+    "recommendCount": 622,
     "disagreeCount": 12,
     "childCount": 11,
     "newsid": "20130606001506524",
