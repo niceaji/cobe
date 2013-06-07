89ea44ba812d4a55c1ab78bbf908c0b9b27888d9
niceaji <a.ji@daum.net>
2013-06-08 06:21:02 +0900

    update Data!

--- data/2013-06-08.js
+++ data/2013-06-08.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 179284647,
+    "name": "쿠키왕자",
+    "userid": "xOeS1cyFMFY0",
+    "content": "야 인간아ㅋㅋㅋ",
+    "time": 1370617722000,
+    "dateStr": "2013-06-08T00:08:42+09:00",
+    "recommendCount": 513,
+    "disagreeCount": 10,
+    "childCount": 0,
+    "newsid": "20130608000508576",
+    "newsTitle": "'나혼자산다' 서인국母, 아들 집 보자마자 \"야 인간아\" 폭발",
+    "newsDate": "20130608000508"
+  }
+]
