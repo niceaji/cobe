3ad240ec696408e15922c852a578e9f2736c6bab
niceaji <a.ji@daum.net>
2013-06-08 07:42:01 +0900

    update Data!

--- data/2013-06-08.js
+++ data/2013-06-08.js
@@ -1,12 +1,26 @@
 [
   {
+    "id": 179285644,
+    "name": "BPM",
+    "userid": "Xki.dErZTy90",
+    "content": "월,화 밤에는 정말 죽여버리고 싶을 정도로 밉고 금욜 밤에는 귀여운 꽃중년이고ㅋㅋㅋㅋㅋㅋㅋㅋ",
+    "time": 1370618345000,
+    "dateStr": "2013-06-08T00:19:05+09:00",
+    "recommendCount": 513,
+    "disagreeCount": 12,
+    "childCount": 1,
+    "newsid": "20130608000116548",
+    "newsTitle": "'나혼자산다' 이성재 \"B1A4 복사용지?\" 아이돌 이름에 멘붕",
+    "newsDate": "20130608000116"
+  },
+  {
     "id": 179284647,
     "name": "쿠키왕자",
     "userid": "xOeS1cyFMFY0",
     "content": "야 인간아ㅋㅋㅋ",
     "time": 1370617722000,
     "dateStr": "2013-06-08T00:08:42+09:00",
-    "recommendCount": 650,
+    "recommendCount": 656,
     "disagreeCount": 14,
     "childCount": 0,
     "newsid": "20130608000508576",
