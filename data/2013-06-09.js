9f35f1f839856ac6830760c263d9e80943364004
niceaji <a.ji@daum.net>
2013-06-09 08:57:02 +0900

    update Data!

--- data/2013-06-09.js
+++ data/2013-06-09.js
@@ -6,7 +6,7 @@
     "content": "동엽신의 아무렇치 않게 툭툭 던지는 능글맞은 멘트는 진짜 갑이다ㅋㅋㅋ",
     "time": 1370705359000,
     "dateStr": "2013-06-09T00:29:19+09:00",
-    "recommendCount": 562,
+    "recommendCount": 569,
     "disagreeCount": 17,
     "childCount": 1,
     "newsid": "20130609002004184",
