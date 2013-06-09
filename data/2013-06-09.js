54688e41eae9c7c5d102244581659d9e1a5026c7
niceaji <a.ji@daum.net>
2013-06-09 09:03:02 +0900

    update Data!

--- data/2013-06-09.js
+++ data/2013-06-09.js
@@ -1,12 +1,26 @@
 [
   {
+    "id": 179413036,
+    "name": "미라쥬대폭탄",
+    "userid": "Sb55qZx_lMo0",
+    "content": "남자가 쓰레기네..\r\n최소한의 책임감도 없냐??\r\n콩심은데 콩난다고 \r\n부모역할이 진짜 중요한걸 요새 새삼 깨닫는다..",
+    "time": 1370730601000,
+    "dateStr": "2013-06-09T07:30:01+09:00",
+    "recommendCount": 533,
+    "disagreeCount": 19,
+    "childCount": 3,
+    "newsid": "20130609043703941",
+    "newsTitle": "억대 지참금 요구에 깨져버린 신혼의 꿈",
+    "newsDate": "20130609043703"
+  },
+  {
     "id": 179400768,
     "name": "축구지존",
     "userid": ".wL8MvzPO-g0",
     "content": "동엽신의 아무렇치 않게 툭툭 던지는 능글맞은 멘트는 진짜 갑이다ㅋㅋㅋ",
     "time": 1370705359000,
     "dateStr": "2013-06-09T00:29:19+09:00",
-    "recommendCount": 579,
+    "recommendCount": 588,
     "disagreeCount": 17,
     "childCount": 1,
     "newsid": "20130609002004184",
