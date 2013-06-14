bd8bf16144ad3c47fd67f4bb6386e7f0c5617709
niceaji <a.ji@daum.net>
2013-06-15 05:48:02 +0900

    update Data!

--- data/2013-06-15.js
+++ data/2013-06-15.js
@@ -6,7 +6,7 @@
     "content": "지금 생각해도 진짜 동생이 생각없던것같다...",
     "time": 1371222566000,
     "dateStr": "2013-06-15T00:09:26+09:00",
-    "recommendCount": 859,
+    "recommendCount": 860,
     "disagreeCount": 72,
     "childCount": 4,
     "newsid": "20130614234804923",
