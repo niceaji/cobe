ac79393f44d2d25f75e14c57557562af4391515d
niceaji <a.ji@daum.net>
2013-06-15 02:03:03 +0900

    update Data!

--- data/2013-06-15.js
+++ data/2013-06-15.js
@@ -6,7 +6,7 @@
     "content": "내동생아닌게다행..진짜예의없는거같단생각...방송에서언니얘기그렇게해놓구 변명은 구구절절..",
     "time": 1371222458000,
     "dateStr": "2013-06-15T00:07:38+09:00",
-    "recommendCount": 569,
+    "recommendCount": 577,
     "disagreeCount": 52,
     "childCount": 2,
     "newsid": "20130614234804923",
