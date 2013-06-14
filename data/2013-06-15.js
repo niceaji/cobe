c09ba2c7b270a1acbce43d35a13c13a7aa21be0b
niceaji <a.ji@daum.net>
2013-06-15 02:09:01 +0900

    update Data!

--- data/2013-06-15.js
+++ data/2013-06-15.js
@@ -1,14 +1,14 @@
 [
   {
-    "id": 180278789,
-    "name": "쯔쯔쯔쯔쯔",
-    "userid": "hQqpi_kiMaM0",
-    "content": "내동생아닌게다행..진짜예의없는거같단생각...방송에서언니얘기그렇게해놓구 변명은 구구절절..",
-    "time": 1371222458000,
-    "dateStr": "2013-06-15T00:07:38+09:00",
-    "recommendCount": 585,
-    "disagreeCount": 52,
-    "childCount": 2,
+    "id": 180278959,
+    "name": "아루",
+    "userid": "Vn3-Qu8C5Ko0",
+    "content": "지금 생각해도 진짜 동생이 생각없던것같다...",
+    "time": 1371222566000,
+    "dateStr": "2013-06-15T00:09:26+09:00",
+    "recommendCount": 565,
+    "disagreeCount": 44,
+    "childCount": 0,
     "newsid": "20130614234804923",
     "newsTitle": "'땡큐' 김성령 \"방송서 불화설 고백..아직도 이해 못하겠다\"",
     "newsDate": "20130614234804"
