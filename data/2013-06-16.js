b57a10d46ada981974176f91f1f5779617c75110
niceaji <a.ji@daum.net>
2013-06-16 08:27:01 +0900

    update Data!

--- data/2013-06-16.js
+++ data/2013-06-16.js
@@ -6,7 +6,7 @@
     "content": "이정도의 오심이면 조작을 의심할만한 상황아닌가?\r\n승부조작이나 사설토토에 연루된건아닌지 조사해봐야는 상황인건같다",
     "time": 1371309894000,
     "dateStr": "2013-06-16T00:24:54+09:00",
-    "recommendCount": 523,
+    "recommendCount": 533,
     "disagreeCount": 10,
     "childCount": 12,
     "newsid": "20130616000904936",
