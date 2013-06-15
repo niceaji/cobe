52a3dcd7616cffb879e430544cc634f6e784160e
niceaji <a.ji@daum.net>
2013-06-16 08:30:02 +0900

    update Data!

--- data/2013-06-16.js
+++ data/2013-06-16.js
@@ -1,12 +1,26 @@
 [
   {
+    "id": 180395023,
+    "name": "ENTZ",
+    "userid": "9MY5TzXeRVI0",
+    "content": "돈 있을때 룸싸롱에 몇백씩 쏟아붓던 사람이 이제와 빚있다고 하면... 그게 불쌍하냐? ",
+    "time": 1371314222000,
+    "dateStr": "2013-06-16T01:37:02+09:00",
+    "recommendCount": 525,
+    "disagreeCount": 5,
+    "childCount": 1,
+    "newsid": "20130616001404962",
+    "newsTitle": "'세바퀴' 이혁재 \"오늘 출연료로 이자 갚는다\"..빚 10억 남아",
+    "newsDate": "20130616001404"
+  },
+  {
     "id": 180389761,
     "name": "혀니",
     "userid": "lxRL54BDNuo0",
     "content": "이정도의 오심이면 조작을 의심할만한 상황아닌가?\r\n승부조작이나 사설토토에 연루된건아닌지 조사해봐야는 상황인건같다",
     "time": 1371309894000,
     "dateStr": "2013-06-16T00:24:54+09:00",
-    "recommendCount": 533,
+    "recommendCount": 544,
     "disagreeCount": 10,
     "childCount": 12,
     "newsid": "20130616000904936",
