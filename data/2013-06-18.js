54297196147a1c24a6258f2b0e1abdb26a1a40d1
niceaji <a.ji@daum.net>
2013-06-18 07:09:01 +0900

    update Data!

--- data/2013-06-18.js
+++ data/2013-06-18.js
@@ -6,7 +6,7 @@
     "content": "딱 보니까 할아버지 가정 내팽개치고 젊었을 때 밖으로 많이 도신듯한데\r\n\r\n지금이라도 부인한테 잘하세요..진짜.....한심합니다...",
     "time": 1371481971000,
     "dateStr": "2013-06-18T00:12:51+09:00",
-    "recommendCount": 572,
+    "recommendCount": 587,
     "disagreeCount": 5,
     "childCount": 0,
     "newsid": "20130618000107932",
