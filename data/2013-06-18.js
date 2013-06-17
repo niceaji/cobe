fc464147665262f3c07a77a0dfdf1fffd10a8b18
niceaji <a.ji@daum.net>
2013-06-18 08:00:02 +0900

    update Data!

--- data/2013-06-18.js
+++ data/2013-06-18.js
@@ -6,12 +6,26 @@
     "content": "응원합니다\r\n\r\n반대는  \r\n\r\n닥조류족들 ?  = 닥대가리들?",
     "time": 1371500352000,
     "dateStr": "2013-06-18T05:19:12+09:00",
-    "recommendCount": 1241,
-    "disagreeCount": 47,
-    "childCount": 24,
+    "recommendCount": 1318,
+    "disagreeCount": 51,
+    "childCount": 28,
     "newsid": "20130618043505448",
     "newsTitle": "서울대생들 '국정원 선거개입' 시국선언 추진",
     "newsDate": "20130618043505"
+  },
+  {
+    "id": 180672949,
+    "name": "Beautiful Star",
+    "userid": "SKdrkNdY79o0",
+    "content": "야하면 돈안받고 안찍으면 되지.. 돈이 그렇게 필요했나?",
+    "time": 1371499761000,
+    "dateStr": "2013-06-18T05:09:21+09:00",
+    "recommendCount": 592,
+    "disagreeCount": 77,
+    "childCount": 13,
+    "newsid": "20130618044205463",
+    "newsTitle": "속옷 모델 자청한 女의대생..뒤늦은 후회",
+    "newsDate": "20130618044205"
   },
   {
     "id": 180662397,
@@ -20,11 +34,25 @@
     "content": "딱 보니까 할아버지 가정 내팽개치고 젊었을 때 밖으로 많이 도신듯한데\r\n\r\n지금이라도 부인한테 잘하세요..진짜.....한심합니다...",
     "time": 1371481971000,
     "dateStr": "2013-06-18T00:12:51+09:00",
-    "recommendCount": 771,
+    "recommendCount": 786,
     "disagreeCount": 6,
     "childCount": 0,
     "newsid": "20130618000107932",
     "newsTitle": "'안녕하세요' 주물러인생 70대 할머니 \"남편 잠들 때까지 안마해야\"",
     "newsDate": "20130618000107"
+  },
+  {
+    "id": 180661792,
+    "name": "ppark",
+    "userid": "OFfQsqgiLDU0",
+    "content": "진정한 힐링의 화신",
+    "time": 1371481546000,
+    "dateStr": "2013-06-18T00:05:46+09:00",
+    "recommendCount": 514,
+    "disagreeCount": 12,
+    "childCount": 1,
+    "newsid": "20130617235605900",
+    "newsTitle": "'힐링캠프' 닉 부이치치 해표지증 \"내가 태어날 때 모두가 울었다\"",
+    "newsDate": "20130617235605"
   }
 ]
