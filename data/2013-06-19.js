d8e29e6274f1004ce4d311082f274d79b552000b
niceaji <a.ji@daum.net>
2013-06-19 05:48:02 +0900

    update Data!

--- data/2013-06-19.js
+++ data/2013-06-19.js
@@ -6,9 +6,9 @@
     "content": "ㅋㅋㅋㅋ 기자가 이동국 안티인가 대놓고 까라고 만든 글같네...",
     "time": 1371570384000,
     "dateStr": "2013-06-19T00:46:24+09:00",
-    "recommendCount": 924,
+    "recommendCount": 930,
     "disagreeCount": 12,
-    "childCount": 17,
+    "childCount": 18,
     "newsid": "20130619004005918",
     "newsTitle": "'A급' 이동국, 월드컵 출전 보너스 1억 받는다",
     "newsDate": "20130619004005"
