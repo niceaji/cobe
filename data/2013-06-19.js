2cb3dbc1df8cbd00a0e51990b30b0fb0e38c83b2
niceaji <a.ji@daum.net>
2013-06-19 02:39:02 +0900

    update Data!

--- data/2013-06-19.js
+++ data/2013-06-19.js
@@ -6,7 +6,7 @@
     "content": "ㅋㅋㅋㅋ 기자가 이동국 안티인가 대놓고 까라고 만든 글같네...",
     "time": 1371570384000,
     "dateStr": "2013-06-19T00:46:24+09:00",
-    "recommendCount": 512,
+    "recommendCount": 518,
     "disagreeCount": 7,
     "childCount": 8,
     "newsid": "20130619004005918",
