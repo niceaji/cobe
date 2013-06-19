11339c78dfef063d4cd31765d3ea423b3cf4ed90
niceaji <a.ji@daum.net>
2013-06-20 08:24:01 +0900

    update Data!

--- data/2013-06-20.js
+++ data/2013-06-20.js
@@ -6,9 +6,9 @@
     "content": "그네씨, 그대는 \r\n       국•정•원•대•통•령 !!!\r\n국민은 그대가 참 부끄러워요\r\n일등공신, 셀프잠금 국정원녀는 지금 어디서 뭐라고 있나요?  포상 두둑히 받아 잘 먹고 잘 살고 있나요?\r\n",
     "time": 1371676091000,
     "dateStr": "2013-06-20T06:08:11+09:00",
-    "recommendCount": 2064,
-    "disagreeCount": 132,
-    "childCount": 75,
+    "recommendCount": 2138,
+    "disagreeCount": 137,
+    "childCount": 77,
     "newsid": "20130620060211394",
     "newsTitle": "[종합]국정원·경찰 선거개입, 대학가 시국선언 확산..도심 곳곳 집회",
     "newsDate": "20130620060211"
