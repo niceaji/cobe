acb5040687ac1544f17575af8498d5074511c2b2
niceaji <a.ji@daum.net>
2013-06-21 07:54:02 +0900

    update Data!

--- data/2013-06-21.js
+++ data/2013-06-21.js
@@ -1,14 +1,28 @@
 [
   {
+    "id": 181237180,
+    "name": "만부기",
+    "userid": "tODojTIY_Nw0",
+    "content": "나라가 이미 나라가 아니다 조작으로 세운 정부는 무효겠지??",
+    "time": 1371762807000,
+    "dateStr": "2013-06-21T06:13:27+09:00",
+    "recommendCount": 543,
+    "disagreeCount": 30,
+    "childCount": 6,
+    "newsid": "20130621040625587",
+    "newsTitle": "\"국정원 선거 개입, 민주주의 훼손\" 대학가 시국선언 확산",
+    "newsDate": "20130621040625"
+  },
+  {
     "id": 181235300,
     "name": "『惡』ZeroS™",
     "userid": "dlQYvp.HMbU0",
     "content": "지겹다...\r\n\r\n진짜 부카니스탄아니면 어케 정치 할거랴...\r\n\r\n근데 이제 물타기용 자료도 많이 딸리나부다\r\n\r\n작년에 써먹다 뽀록난걸 또 써먹어?\r\n\r\n왜 요새 물타기 잘 안먹히지?\r\n\r\n연예기사 뻥뻥 터트려도 사람들이 별로 관심도 안갖고...\r\n\r\n민주당이 동의 안해줘서 못깐다고?\r\n\r\n까구 앉아 있네 언제는 동의해줘서\r\n\r\nFTA,미디어법 같은거 날치기 했냐?",
     "time": 1371759018000,
     "dateStr": "2013-06-21T05:10:18+09:00",
-    "recommendCount": 927,
-    "disagreeCount": 65,
-    "childCount": 35,
+    "recommendCount": 1010,
+    "disagreeCount": 72,
+    "childCount": 41,
     "newsid": "20130621040619564",
     "newsTitle": "與 \"민주당도 봤으면 기겁할 것\"  野 \"대선 불법 개입 물타기\"",
     "newsDate": "20130621040619"
