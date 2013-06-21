7f3916ffdf7bedbc3a5f8db1e86fc00a63573101
niceaji <a.ji@daum.net>
2013-06-22 08:15:02 +0900

    update Data!

--- data/2013-06-22.js
+++ data/2013-06-22.js
@@ -6,9 +6,9 @@
     "content": "이러고 은근슬쩍 국정원관련 사건묻히려고?아오 열받네 nll대화록보다 국정원 정치개입이 지금 더 중요한거아니야?",
     "time": 1371853388000,
     "dateStr": "2013-06-22T07:23:08+09:00",
-    "recommendCount": 619,
-    "disagreeCount": 38,
-    "childCount": 34,
+    "recommendCount": 674,
+    "disagreeCount": 39,
+    "childCount": 35,
     "newsid": "20130622032215021",
     "newsTitle": "메가톤급 파장.. 정치게임 최종 승자는?",
     "newsDate": "20130622032215"
