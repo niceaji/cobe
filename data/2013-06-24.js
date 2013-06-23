a23dbb0b62eaade6de9923b9b2e348b883dba3d6
niceaji <a.ji@daum.net>
2013-06-24 08:00:02 +0900

    update Data!

--- data/2013-06-24.js
+++ data/2013-06-24.js
@@ -6,9 +6,9 @@
     "content": "박근혜 새누리당놈들 이따위로 추잡하게 나오냐?",
     "time": 1372020753000,
     "dateStr": "2013-06-24T05:52:33+09:00",
-    "recommendCount": 950,
-    "disagreeCount": 78,
-    "childCount": 27,
+    "recommendCount": 977,
+    "disagreeCount": 80,
+    "childCount": 29,
     "newsid": "20130624024133140",
     "newsTitle": "'국정원 사건' 진보 촛불 vs 보수 맞불 집회",
     "newsDate": "20130624024133"
