fc6de7d021050abbd519e8a18743fca972b0e9f4
niceaji <a.ji@daum.net>
2013-06-24 07:24:02 +0900

    update Data!

--- data/2013-06-24.js
+++ data/2013-06-24.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 181654406,
+    "name": "언덕바람",
+    "userid": "Fx6FHSuJKoE0",
+    "content": "박근혜 새누리당놈들 이따위로 추잡하게 나오냐?",
+    "time": 1372020753000,
+    "dateStr": "2013-06-24T05:52:33+09:00",
+    "recommendCount": 560,
+    "disagreeCount": 54,
+    "childCount": 17,
+    "newsid": "20130624024133140",
+    "newsTitle": "'국정원 사건' 진보 촛불 vs 보수 맞불 집회",
+    "newsDate": "20130624024133"
+  }
+]
