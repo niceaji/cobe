a93b6c6c08423e0c10ef5ff01e8d91607de78dab
niceaji <a.ji@daum.net>
2013-06-26 06:00:02 +0900

    update Data!

--- data/2013-06-26.js
+++ data/2013-06-26.js
@@ -1,1 +1,16 @@
-[]
+[
+  {
+    "id": 182095233,
+    "name": "kwanz",
+    "userid": "rMLAM9M96uQ0",
+    "content": "이걸 금지령으로\r\n내려야 안하는 사람들...\r\n\r\n평소에는 도대체 어떨까?",
+    "time": 1372172959000,
+    "dateStr": "2013-06-26T00:09:19+09:00",
+    "recommendCount": 508,
+    "disagreeCount": 7,
+    "childCount": 11,
+    "newsid": "20130625211706916",
+    "newsTitle": "\"제2 윤창중 막자\"..수행단 음주·발마사지 금지령",
+    "newsDate": "20130625211706"
+  }
+]
