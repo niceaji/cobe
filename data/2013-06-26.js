fb0f81e2fb8bb0a5bde2c794f1ba7217b9b8741c
niceaji <a.ji@daum.net>
2013-06-26 06:36:01 +0900

    update Data!

--- data/2013-06-26.js
+++ data/2013-06-26.js
@@ -6,7 +6,7 @@
     "content": "이걸 금지령으로\r\n내려야 안하는 사람들...\r\n\r\n평소에는 도대체 어떨까?",
     "time": 1372172959000,
     "dateStr": "2013-06-26T00:09:19+09:00",
-    "recommendCount": 575,
+    "recommendCount": 580,
     "disagreeCount": 7,
     "childCount": 13,
     "newsid": "20130625211706916",
