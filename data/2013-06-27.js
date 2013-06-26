a9bb5bf7c566ff74df0a1a90c49f752b2fb53bd4
niceaji <a.ji@daum.net>
2013-06-27 06:15:02 +0900

    update Data!

--- data/2013-06-27.js
+++ data/2013-06-27.js
@@ -6,9 +6,9 @@
     "content": "북한한테도  까이는 새머리당\r\n\r\n참  국격돋네......!!\r\n\r\n야덜아 총풍사건부터  달구족의 최고 존엄\r\n\r\n다까끼 마사오상의  딸  니네  북에가서\r\n\r\n뭐했는지 속시원히 까주면  안되겠니?",
     "time": 1372274458000,
     "dateStr": "2013-06-27T04:20:58+09:00",
-    "recommendCount": 787,
-    "disagreeCount": 84,
-    "childCount": 27,
+    "recommendCount": 819,
+    "disagreeCount": 90,
+    "childCount": 32,
     "newsid": "20130627040908560",
     "newsTitle": "北, 대화록 공개에 \"최고존엄 우롱\" 비난(종합)",
     "newsDate": "20130627040908"
